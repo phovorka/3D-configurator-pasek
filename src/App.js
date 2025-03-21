e1899faa71e034e37e99bfbd1934fbf2ce86e520
phovorka <hovorkapetr@seznam.cz>
2025-03-21 18:00:15 +0100

    75

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var jsx_runtime_1 = require("react/jsx-runtime");
 var react_1 = require("react");
 require("@google/model-viewer");
-require("./App.css"); // Ujisti se, že máš vlastní styly v tomto souboru
+require("./App.css");
 var WatchConfigurator = function () {
     var modelRef = (0, react_1.useRef)(null);
     (0, react_1.useEffect)(function () {
@@ -33,6 +33,6 @@ var WatchConfigurator = function () {
         };
         return colors[color] || [1, 1, 1, 1];
     };
-    return ((0, jsx_runtime_1.jsxs)("div", { className: "watch-configurator", children: [(0, jsx_runtime_1.jsx)("h3", { className: "title", children: "3D Konfigur\u00E1tor p\u00E1sku" }), (0, jsx_runtime_1.jsx)("model-viewer", { ref: modelRef, src: "https://cdn.jsdelivr.net/gh/phovorka/GLB/watch1.glb", "camera-controls": true, "auto-rotate": true }), (0, jsx_runtime_1.jsx)("div", { className: "color-picker", children: ['black', 'brown', 'blue', 'red'].map(function (color) { return ((0, jsx_runtime_1.jsx)("button", { className: "color-button", style: { backgroundColor: color }, onClick: function () { return changeColor(color); } }, color)); }) })] }));
+    return ((0, jsx_runtime_1.jsxs)("div", { className: "watch-configurator", children: [(0, jsx_runtime_1.jsx)("h3", { className: "title", children: "3D Konfigur\u00E1tor p\u00E1sku" }), (0, jsx_runtime_1.jsx)("model-viewer", { ref: modelRef, src: "https://cdn.jsdelivr.net/gh/phovorka/GLB/watch1.glb", "camera-controls": true, "auto-rotate": true }), " as any;", (0, jsx_runtime_1.jsx)("div", { className: "color-picker", children: ['black', 'brown', 'blue', 'red'].map(function (color) { return ((0, jsx_runtime_1.jsx)("button", { className: "color-button", style: { backgroundColor: color }, onClick: function () { return changeColor(color); } }, color)); }) })] }));
 };
 exports.default = WatchConfigurator;
